7c1a4782188c0d166dc06b1553d86ea90b8863b4
Michael <gendm@yahoo.com>
2024-12-28 12:06:11 +0200

    First task

---
+++ Homework-1/src/task-3.cpp
@@ -0,0 +1,9 @@
+include<iostream> // here "#" is missed
+
+    using namespace std;
+
+int main()
+{
+    cout << "Hello World\n";
+    return 0;
+}
---
+++ Homework-1/src/task-4.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World\n"; // outputs to console "Hello World"
+    return 0;
+}
