3e006ac8b4d99ae2fe6116008c3defe12e787a14
Michael <gendm@yahoo.com>
2025-01-04 11:14:48 +0200

    Homework-2

---
+++ Homework-2/src/ILoveCpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "I Love C++" << std::endl;
+    return 0;
+}
---
+++ Homework-2/src/w-Bug.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // obviosly a quote symbol is missed in the beginning of string expression and ";" in the end
+    cout << Is there a bug here ? " 
+        return 0;
+}
