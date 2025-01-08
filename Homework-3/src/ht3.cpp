02304a1ab0235accd61dc8a03361f77493b38420
Michael <gendm@yahoo.com>
2025-01-08 20:46:55 +0200

    Homework-3

---
+++ Homework-3/src/ht3.cpp
@@ -0,0 +1,11 @@
+
+#define PI 3.141592;
+
+int main()
+{
+    unsigned char myAge;                 // i have a great plans
+    float backYardArea;                  // but not such a big property
+    unsigned long int galaxyStarsAmount; // for 1-4 * 10^10 must be enough
+    float avgJanuaryRaining;             // not such a big number but average must be float
+    float pi = PI;
+}
